ab3b7b91525eb59d88086feda5170fb370ae140f
Willis <3475947+JasherIO@users.noreply.github.com>
2018-06-29 09:35:56 -0500

    SEO, RSS, and Markdown utility plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,65 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Jasher',
+    description: 'Jasher: Software. Events. Articles.',
+    siteUrl: 'https://jasher.io'
   },
   plugins: [
+    'gatsby-plugin-catch-links',
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  limit: 1000,
+                  sort: { order: DESC, fields: [frontmatter___date] }
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+          },
+        ],
+      },
+    },
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-robots-txt',
     'gatsby-plugin-sass',
     {
       resolve: 'gatsby-source-filesystem',
@@ -19,12 +75,26 @@ module.exports = {
         name: 'images',
       },
     },
+    'gatsby-plugin-sitemap',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     {
       resolve: 'gatsby-transformer-remark',
       options: {
-        plugins: [],
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 690
+            },
+          },
+          {
+            resolve: "gatsby-remark-responsive-iframe"
+          },
+          `gatsby-remark-prismjs`,
+          "gatsby-remark-copy-linked-files",
+          `gatsby-remark-autolink-headers`
+        ],
       },
     },
     {
