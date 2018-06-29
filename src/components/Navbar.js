230f22ad9782029232ea4fccb18e6f158171dbdf
Willis <3475947+JasherIO@users.noreply.github.com>
2018-06-29 09:37:30 -0500

    Burger, social links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,34 +5,48 @@ import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
 const Navbar = () => (
-  <nav className="navbar is-transparent">
+  <nav className="navbar">
     <div className="container">
       <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
+        {/* TODO: Link to / */}
+        <Link to='/' className="navbar-item">
+          <img src="https://bulma.io/images/bulma-type-white.png" alt="Logo"></img>
         </Link>
+        <span className="navbar-burger burger" data-target="navbarMenuHeroA">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
       </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
+      <div id="navbarMenuHeroA" className="navbar-menu">
+        <div className="navbar-end">
+          <Link to="/about" className="navbar-item">
+            About
+          </Link>
+          <Link to="/blog" className="navbar-item">
+            Blog
+          </Link>
+          <a className="navbar-item">
+            <span className="icon">
+              <i className="fab fa-lg fa-github" aria-hidden="true"></i>
+            </span>
+          </a>
+          <a className="navbar-item">
+            <span className="icon">
+              <i className="fab fa-lg fa-linkedin" aria-hidden="true"></i>
+            </span>
+          </a>
+          <a className="navbar-item">
+            <span className="icon">
+              <i className="fab fa-lg fa-twitter" aria-hidden="true"></i>
+            </span>
+          </a>
+          <a className="navbar-item">
+            <span className="icon">
+              <i className="fab fa-lg fa-twitch" aria-hidden="true"></i>
+            </span>
+          </a>
+        </div>
       </div>
     </div>
   </nav>
