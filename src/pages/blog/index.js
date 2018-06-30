7b71669b4addaa7f6fe813d772cbe158bf806d20
Willis <3475947+JasherIO@users.noreply.github.com>
2018-06-30 16:51:02 -0500

    Page titles

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
 import _ from 'lodash'
 
@@ -7,24 +8,28 @@ export default class IndexPage extends React.Component {
   render() {
     const { data } = this.props
     const { edges: posts } = data.posts
+    const { title } = data.site.siteMetadata
     const { group } = data.tags
     const tags = _.sortBy(_.each(group, (o) => { o.fieldValue = _.startCase(o.fieldValue) }), 'fieldValue')
 
     return (
       <section className="section">
+        <Helmet title={`Blog | ${title}`} />
         <div className="columns">
           <div className="column is-6 is-offset-2">
             {/* <p className="is-size-3 has-text-weight-bold">Recent posts</p> */}
             {posts
             .map(({ node: post }) => (
-              <div style={{ padding: '1em 2em' }}>
+              <div 
+                style={{ padding: '1em 2em' }}
+                key={post.id}>
                 <Link to={post.fields.slug}>
-                  <p class="is-size-4 has-text-weight-semibold">{post.frontmatter.title}</p>
+                  <p className="is-size-4 has-text-weight-semibold">{post.frontmatter.title}</p>
                 </Link>
                 <div className="content">
                   {_.isEmpty(post.frontmatter.description) ? post.excerpt : post.frontmatter.description}
                   <br />
-                  <time datetime="post.frontmatter.date">{post.frontmatter.date}</time>
+                  <time dateTime="post.frontmatter.date">{post.frontmatter.date}</time>
                   <span>&nbsp;&bull;&nbsp;{post.timeToRead} minute read</span>
                 </div>
               </div>
@@ -35,9 +40,9 @@ export default class IndexPage extends React.Component {
               <p className="menu-label">
                 Tags
               </p>
-              <ul class="menu-list">
+              <ul className="menu-list">
                 {tags.map((tag) => (
-                  <li>
+                  <li key={tag.fieldValue}>
                     <Link to={`/tags/${_.kebabCase(tag.fieldValue)}`}>
                       {tag.fieldValue} ({tag.totalCount})
                     </Link>
@@ -54,6 +59,7 @@ export default class IndexPage extends React.Component {
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
+    site: PropTypes.object,
     posts: PropTypes.shape({
       edges: PropTypes.array,
     }),
@@ -65,6 +71,11 @@ IndexPage.propTypes = {
 
 export const pageQuery = graphql`
   query PostsQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
     posts: allMarkdownRemark(
       sort: { order: DESC, fields: [frontmatter___date] },
       filter: { frontmatter: { templateKey: { eq: "blog-post" } }}
--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Content, { HTMLContent } from '../components/Content'
+import Helmet from 'react-helmet'
 
 export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content
 
   return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
-              <PageContent className="content" content={content} />
-            </div>
+    <section className="section">
+      <Helmet title="About | Jasher" />
+      <div className="columns">
+        <div className="column is-6 is-offset-2">
+          <div className="section">
+            <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+              {title}
+            </h2>
+            <PageContent className="content" content={content} />
           </div>
         </div>
       </div>
