a33212da3666dcc958f46f3ab5520857d31fb02e
Willis <3475947+JasherIO@users.noreply.github.com>
2018-11-13 08:13:13 -0600

    Temp home page for publishing

--- src/components/Home/Bio.js
+++ src/components/Home/Bio.js
@@ -3,11 +3,11 @@ import React from 'react'
 const Bio = ({ ...props }) => (
   <section className="section is-medium has-background-primary has-text-centered" {...props}>
     <div className="container">
-      <h1 className="title is-3 has-text-white ">
+      <h1 className="title is-3">
         Hi! I'm Josh!
       </h1>
-      <h1 className="subtitle is-5 has-text-white ">
-        I am a software developer with an interest in techonology, video games, sports, esports.
+      <h1 className="subtitle is-5">
+        I am a software developer with an interest in technology, video games, and esports.
       </h1>
     </div>
   </section>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import Bio from '../components/Home/Bio'
-import Software from '../components/Home/Software'
-import Events from '../components/Home/Events'
-import Writing from '../components/Home/Writing'
+// import Software from '../components/Home/Software'
+// import Events from '../components/Home/Events'
+// import Writing from '../components/Home/Writing'
 
 export default class IndexPage extends React.Component {
   render() {
     return (
       <>
         <Bio />
-        <Software />
-        <Events />
-        <Writing />
+        {/* <Software /> */}
+        {/* <Events /> */}
+        {/* <Writing /> */}
       </>
     )
   }
